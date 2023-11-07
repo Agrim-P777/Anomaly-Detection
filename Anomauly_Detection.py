77ae85e5af47efe17e7fa8738e76166e3a6149e7
Agrim-P777 <105589714+Agrim-P777@users.noreply.github.com>
2023-11-07 18:57:57 +0530

    Add files via upload

---
+++ Anomauly_Detection.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+
+%matplotlib inline
+
+# Load the dataset
+X_train, X_val, y_val = load_data()
+
+
+def estimate_gaussian(X): 
+   m, n = X.shape
+   """
+    Calculates mean and variance of all features 
+    in the dataset
+    
+    Args:
+        X (ndarray): (m, n) Data matrix
+    
+    Returns:
+        mu (ndarray): (n,) Mean of all features
+        var (ndarray): (n,) Variance of all features
+   """
+ 
+   mu =1 / m * np.sum(X, axis = 0)
+   var = 1 / m * np.sum((X - mu) ** 2, axis = 0) 
+   
+
+   return mu, var
+
+
+def select_threshold(y_val, p_val): 
+   best_epsilon = 0
+   best_F1 = 0
+   F1 = 0
+   """
+    Finds the best threshold to use for selecting outliers 
+    based on the results from a validation set (p_val) 
+    and the ground truth (y_val)
+    
+    Args:
+        y_val (ndarray): Ground truth on validation set
+        p_val (ndarray): Results on validation set
+        
+    Returns:
+        epsilon (float): Threshold chosen 
+        F1 (float):      F1 score by choosing epsilon as threshold
+    """ 
+   
+
+   step_size = (max(p_val) - min(p_val)) / 1000
+
+   for epsilon in np.arange(min(p_val), max(p_val), step_size):
+
+       predictions = (p_val < epsilon)
+
+       tp = np.sum((predictions == 1) & (y_val == 1))
+       fp = sum((predictions == 1) & (y_val == 0))
+       fn = np.sum((predictions == 0) & (y_val == 1))
+
+       prec = tp / (tp + fp)
+       rec = tp / (tp + fn)
+
+       F1 = 2 * prec * rec / (prec + rec)
+       
+
+       if F1 > best_F1:
+           best_F1 = F1
+           best_epsilon = epsilon
+
+   return best_epsilon, best_F1
